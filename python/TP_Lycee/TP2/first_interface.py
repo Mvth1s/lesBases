b72050c43e31c56f512504c800c6832d2224a2e9
Mvth1s <aguadomathis@gmail.com>
2026-02-17 10:38:34 +0100

    Python: tp2 ex1 part3

--- python/TP_Lycee/TP2/first_interface.py
+++ python/TP_Lycee/TP2/first_interface.py
@@ -7,7 +7,7 @@ fenetre.title('Coucou')
 texte = Label(fenetre, text = 'Bonjour tout le monde !', fg = 'red')
 texte.pack()
 bouton = Button(fenetre, text = 'Quitter', command = fenetre.destroy)
-bouton.pack
+bouton.pack()
 fenetre.mainloop()
  
 # ----------- Part 2 ---------- #
@@ -19,5 +19,17 @@ texte.pack()
 texte2 = Label(fenetre, text = 'Comment ça va ?', fg = '#0000FF')
 texte2.pack()
 bouton = Button(fenetre, text = 'Quitter', command = fenetre.destroy)
-bouton.pack
+bouton.pack()
+fenetre.mainloop()
+
+# ----------- Part 3 ---------- #
+
+fenetre = Tk()
+fenetre.title("Part 3")
+texte = Label(fenetre, text = "Hello Python !", fg = "#10b981")
+texte.pack()
+texte2 = Label(fenetre, text = "Les bases de Python d'après mes cours de Lycée !")
+texte2.pack()
+bouton = Button(fenetre, text = "Quitter", command = fenetre.destroy)
+bouton.pack()
 fenetre.mainloop()
