617826c8a44b6ccc4b910b794a966afdca3e6229
Mvth1s <aguadomathis@gmail.com>
2026-02-17 10:33:53 +0100

    Python: tp2 ex1 part2

--- python/TP_Lycee/TP2/first_interface.py
+++ python/TP_Lycee/TP2/first_interface.py
@@ -1,9 +1,23 @@
 from tkinter import Tk, Label, Button
 
+# ----------- Part 1 ---------- #
+
 fenetre = Tk()
 fenetre.title('Coucou')
 texte = Label(fenetre, text = 'Bonjour tout le monde !', fg = 'red')
 texte.pack()
+bouton = Button(fenetre, text = 'Quitter', command = fenetre.destroy)
+bouton.pack
+fenetre.mainloop()
+ 
+# ----------- Part 2 ---------- #
+
+fenetre = Tk()
+fenetre.title('Coucou')
+texte = Label(fenetre, text = 'Bonjour tout le monde !', fg = 'red')
+texte.pack()
+texte2 = Label(fenetre, text = 'Comment ça va ?', fg = '#0000FF')
+texte2.pack()
 bouton = Button(fenetre, text = 'Quitter', command = fenetre.destroy)
 bouton.pack
 fenetre.mainloop()
